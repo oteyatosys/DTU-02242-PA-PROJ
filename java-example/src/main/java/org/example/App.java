98ec1a1ef60316acb4b647ba675ef14202c8f3f0
Michael Andersen <michael@otey.dk>
2024-10-30 12:35:22 +0100

    Extend sample app

--- java-example/src/main/java/org/example/App.java
+++ java-example/src/main/java/org/example/App.java
@@ -1,8 +1,21 @@
 package org.example;
 
 public class App {
-    
+
+    public static String getGreeting(String greeting, String name){
+        return greeting + ", " + name + "!";
+    }
+
+    public static String getGreeting(String name){
+        return getGreeting("Hello", name);
+    }
+
+    // We make a test method that also utilizes geneics
+    public static <T> String getGreeting(String greeting, T name){
+        return greeting + ", " + name + "!";
+    }
+
     public static void main( String[] args ){
-        System.out.println( "Yo World!" );
+        System.out.println( getGreeting("Yo", "World") );
     }
 }
