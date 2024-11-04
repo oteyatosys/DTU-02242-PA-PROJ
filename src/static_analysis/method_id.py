7009bf0d0cb8cce49ea158c8f45aa1c1a6a61cab
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2024-11-04 14:31:20 +0100

    Make call to parse_params in utils

--- src/static_analysis/method_id.py
+++ src/static_analysis/method_id.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
-from jpamb_utils import JvmType, parse_params as jpamb_parse_params
+import jpamb_utils
+from jpamb_utils import JvmType
 from typing import Optional
 import sys, logging
 
 l = logging
-l.basicConfig(level=logging.DEBUG, format="%(message)s")
 
 @dataclass(frozen=True)
 class MethodId:
@@ -27,39 +27,15 @@ class MethodId:
         return MethodId(
             class_name=i["class_name"].replace('.','/'),
             method_name=i["method_name"],
-            params=jpamb_parse_params(i["params"]),
-            return_type=None if i["return"] == "V" else jpamb_parse_params([i["return"]])[0],
+            params=jpamb_utils.parse_params(i["params"]),
+            return_type=None if i["return"] == "V" else jpamb_utils.parse_params([i["return"]])[0],
         )
     
-    @staticmethod
-    def parse_params(input_type: str) -> tuple[JvmType]:
-        params = []
-        while input_type:
-            (tt, input_type) = MethodId.parse_type(input_type)
-            params.append(tt)
-
-        return tuple(params)
-
-    @staticmethod
-    def parse_type(input_type: str) -> tuple[JvmType, str]:
-        assert input_type
-        TYPE_LOOKUP: dict[str, JvmType] = {
-            "Z": "boolean",
-            "I": "int",
-            "C": "char",
-            "[I": "int[]",
-            "[C": "char[]",
-        }
-
-        if input_type[0] in TYPE_LOOKUP:
-            return (TYPE_LOOKUP[input_type[0]], input_type[1:])
-        elif input_type[0] == "[":  # ]
-            return (TYPE_LOOKUP[input_type[:2]], input_type[2:])
-        else:
-            raise ValueError(f"Unknown type {input_type}")
+    def params_to_str(self) -> str:
+        return ','.join(map(str, self.params))
 
     def fully_qualified_signature(self) -> str:
-        return f"{self.class_name}/{self.method_name}({','.join(self.params)})"
+        return f"{self.class_name}/{self.method_name}({self.params_to_str()})"
 
     @staticmethod
     def from_bytecode_invocation(bytecode: dict) -> 'MethodId':
