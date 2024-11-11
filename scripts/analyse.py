39c8ff443835d5a89790759cb154ebfaec35a9e9
Michael Andersen <michael@otey.dk>
2024-11-11 16:53:43 +0100

    Changes

--- scripts/analyse.py
+++ scripts/analyse.py
@@ -8,15 +8,18 @@ import subprocess
 project_root = Path(__file__).parent.parent
 sys.path.append(str(project_root / "src"))
 
-program_dir: Path = project_root / "java-example"
-
 from reader.program import Program
 from syntactic_analysis.scanner import get_int_literals
 
 def load_program() -> Program:
     bytecode_dir = project_root / "data" / "bytecode"
     source_dir = project_root / "data" / "source"
-    return Program.scan(source_dir, bytecode_dir)
+    test_source_dir = project_root / "data" / "test_source"
+    test_bytecode_dir = project_root / "data" / "test_bytecode"
+
+    return Program() \
+        .scan(source_dir, bytecode_dir) \
+        .scan_tests(test_source_dir, test_bytecode_dir)
 
 def prepare_data():
     subprocess.run([str(project_root / "prepare_data.sh")])
