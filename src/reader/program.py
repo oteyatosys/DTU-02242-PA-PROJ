fbb78f9d175fb024ff6d70476457f1b5389c3dd9
Michael Andersen <michael@otey.dk>
2024-11-11 16:18:06 +0100

    Add test sources and test bytecode to Program

--- src/reader/program.py
+++ src/reader/program.py
@@ -2,17 +2,26 @@ from reader.file import File
 from pathlib import Path
 
 class Program:
-    def __init__(self, files: dict[str, File]):
+    def __init__(self, files: dict[str, File] = {}, test_files: dict[str, File] = {}):
         self.files = files
+        self.test_files = test_files
 
     @staticmethod
     def scan(source_root_path: Path, bytecode_root_path: Path) -> "Program":
-        files = {}
+        return Program().scan(source_root_path, bytecode_root_path)
 
+    def scan(self, source_root_path: Path, bytecode_root_path: Path):
         # Walk the source directory and assume that paths are the same for the bytecode
         for source_file in source_root_path.rglob("*.java"):
             bytecode_file = bytecode_root_path / source_file.relative_to(source_root_path).with_suffix(".json")
             file = File.from_path(source_file, bytecode_file)
-            files[file.name] = file
-
-        return Program(files)
+            self.files[file.name] = file
+        return self
+    
+    def scan_tests(self, test_source_root_path: Path, test_bytecode_root_path: Path):
+        # Walk the test source directory and assume that paths are the same for the test bytecode
+        for source_file in test_source_root_path.rglob("*.java"):
+            bytecode_file = test_bytecode_root_path / source_file.relative_to(test_source_root_path).with_suffix(".json")
+            file = File.from_path(source_file, bytecode_file)
+            self.test_files[file.name] = file
+        return self
