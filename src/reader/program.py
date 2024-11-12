aa812f68edb3572a71ba3d9e508dff630343d42c
Michael Andersen <michael@otey.dk>
2024-11-12 13:57:07 +0100

    Update java-example

--- src/reader/program.py
+++ src/reader/program.py
@@ -10,27 +10,27 @@ class Program:
         self.test_files = test_files
 
     @staticmethod
-    def scan(source_root_path: Path, bytecode_root_path: Path) -> "Program":
-        return Program().scan(source_root_path, bytecode_root_path)
 
     def scan(self, source_root_path: Path, bytecode_root_path: Path):
         # Walk the source directory and assume that paths are the same for the bytecode
+    def scan_files(target: dict[str, File], source_root_path: Path, bytecode_root_path: Path):
         for source_file in source_root_path.rglob("*.java"):
             bytecode_file = bytecode_root_path / source_file.relative_to(source_root_path).with_suffix(".json")
             file = File.from_path(source_file, bytecode_file)
-            self.files[file.name] = file
-        return self
-    
-    def scan_tests(self, test_source_root_path: Path, test_bytecode_root_path: Path):
-        # Walk the test source directory and assume that paths are the same for the test bytecode
-        for source_file in test_source_root_path.rglob("*.java"):
             bytecode_file = test_bytecode_root_path / source_file.relative_to(test_source_root_path).with_suffix(".json")
             file = File.from_path(source_file, bytecode_file)
-            self.test_files[file.name] = file
-        return self
+            target[file.name] = file
+
+    @staticmethod
+    def load(data_dir: Path):
+        return program
 
     def all_methods(self) -> Generator[Tuple[File, Method], None, None]:
         for file in self.files.values():
+            for method in file.methods.values():
+                yield file, method
+
+        for file in self.test_files.values():
             for method in file.methods.values():
                 yield file, method
 
