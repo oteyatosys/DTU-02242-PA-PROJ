fcff01c38492d41c536eba5858d79e02de62f267
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2024-11-15 20:14:45 +0100

    Add factorial test

--- java-example/src/test/java/org/example/AppTest.java
+++ java-example/src/test/java/org/example/AppTest.java
@@ -1,17 +1,25 @@
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
     @Test
-    public void testThruth( ){
+    public static void testThruth( ){
         assertTrue( true );
     }
 
     @Test
-    public void testFail( ){
+    public static void testFail( ){
         assertTrue( false );
+    }
+
+    @Test
+    public static void testFactorial() {
+        assert App.factorial(0) == 1;
+        assert App.factorial(1) == 1;
+        assert App.factorial(2) == 2;
+        assert App.factorial(3) == 6;
     }
 }
