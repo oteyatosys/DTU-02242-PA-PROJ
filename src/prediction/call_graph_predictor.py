d44dca432aab74b1da21150c98508a0f1f924495
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2024-11-16 23:42:17 +0100

    Update predictor interface

--- src/prediction/call_graph_predictor.py
+++ src/prediction/call_graph_predictor.py
@@ -11,9 +11,6 @@ from syntactic_analysis.bytecode.call_graph import build_call_graph
 
 @dataclass
 class CallGraphPredictor(TestPredictor):
-    old_program: Program
-    new_program: Program
-
     # Used to remove offsets from bytecode, as these seem to alternate between decompilations
     def _remove_offsets(self, bytecode):
         if isinstance(bytecode, dict):
@@ -22,8 +19,8 @@ class CallGraphPredictor(TestPredictor):
             return [self._remove_offsets(item) for item in bytecode]
         return bytecode
 
-    def predict(self) -> Set[MethodSignature]:
-        call_graph = build_call_graph(self.old_program)
+    def predict(self, old_program: Program, new_program: Program) -> Set[MethodSignature]:
+        call_graph = build_call_graph(old_program)
         
         # draw_graph(call_graph)
 
@@ -34,14 +31,14 @@ class CallGraphPredictor(TestPredictor):
 
         def walk_callgraph(start_node: MethodSignature, old_signature: MethodSignature) -> bool:
 
-            if not self.new_program.contains_method(old_signature):
+            if not new_program.contains_method(old_signature):
                 print(f"Method {old_signature} has been removed")
                 test_predictions.add(start_node)
                 new_methods.add(old_signature)
                 return False
             
-            new_method = self.new_program.method(old_signature)
-            old_method = self.old_program.method(old_signature)
+            new_method = new_program.method(old_signature)
+            old_method = old_program.method(old_signature)
 
             new_bytecode = self._remove_offsets(new_method.bytecode)
             old_bytecode = self._remove_offsets(old_method.bytecode)
@@ -59,9 +56,11 @@ class CallGraphPredictor(TestPredictor):
 
             return True
 
-        for file, method in self.new_program.all_test_methods():
+        for file, method in new_program.all_test_methods():
             if not method.signature in call_graph:
                 test_predictions.add(method.signature)
                 continue
             
             call_graph.bfs_walk(method.signature, walk_callgraph)
+
+        return test_predictions
--- src/prediction/predictor.py
+++ src/prediction/predictor.py
@@ -3,8 +3,9 @@ from abc import ABC, abstractmethod
 from typing import Set
 
 from reader.method_signature import MethodSignature
+from reader.program import Program
 
 class TestPredictor(ABC):
     @abstractmethod
-    def predict(self) -> Set[MethodSignature]:
+    def predict(self, old_program: Program, new_program: Program) -> Set[MethodSignature]:
         pass
