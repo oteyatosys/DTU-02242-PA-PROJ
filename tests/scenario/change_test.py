01027d23b4e3a4900bb094eef39c4789ed4d6d8e
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2024-11-16 23:40:53 +0100

    Update test

--- tests/scenario/change_test.py
+++ tests/scenario/change_test.py
@@ -1,11 +1,12 @@
 from pathlib import Path
-from evaluation.test_scenario import Change, TestStage
+from evaluation.test_scenario import Addition, Deletion, TestStage
 
 def test_apply_and_revert_changes(tmp_path: Path):
     # Setup
     base_path: Path = tmp_path / "base_code"
     base_path.mkdir()
-    file_to_edit = base_path / "existing_file.py"
+    file_rel = Path("existing_file.py")
+    file_to_edit = base_path / file_rel
     original_content = """line 1
 line 2
 line 3
@@ -17,28 +18,30 @@ line 6
 
     # Create TestScenario
     test_scenario = TestStage([
-            Change(
-                file_to_edit, (1, 2), 
-"""
-print('Hello World')
-"""),
-            Change(
-                file_to_edit, (4, 4), 
-                "")
+            Addition(
+                file_rel, 0, 
+                "print('Hello World')\n"),
+            Deletion(
+                file_rel, (4, 5)),
+            Addition(
+                file_rel, 6,
+                "Hej\n"),
         ])
 
     # Apply changes
-    test_scenario.apply_changes()
+    test_scenario.apply_changes(base_path)
     modified_content = file_to_edit.read_text()
     expected_modified_content = """print('Hello World')
+line 1
+line 2
 line 3
-line 5
 line 6
+Hej
 """
     assert modified_content == expected_modified_content
 
     # Revert changes
-    test_scenario.revert_changes()
+    test_scenario.revert_changes(base_path)
     reverted_content = file_to_edit.read_text()
 
     assert reverted_content == original_content
