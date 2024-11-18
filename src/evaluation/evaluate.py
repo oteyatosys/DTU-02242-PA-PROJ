fb5ab043a12347699fa88a7a90fa7f7eb3b5d35b
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2024-11-18 13:11:41 +0100

    Add project root path and print average time statistics in evaluation

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -6,6 +6,7 @@ from prediction.call_graph_predictor import CallGraphPredictor
 from prediction.predictor import TestPredictor
 from reader.method_signature import MethodSignature
 
+project_root = Path(__file__).parent.parent.parent
 
 def main():
     evaluator = Evaluator()
@@ -13,7 +14,7 @@ def main():
     predictor: TestPredictor = CallGraphPredictor()
 
     test_scenario1 = TestScenario(
-        Path("java-example"), [
+        project_root / "java-example", [
         TestStage([
             ReplaceLines(
                 Path("org/example/App.java"), (20, 20),
@@ -27,12 +28,17 @@ def main():
     ])
 
     test_suite = TestSuite([
-        test_scenario1
+        test_scenario1,
     ])
     
     result = evaluator.evaluate_suite(predictor, test_suite)
 
     result.print_stats()
+
+    avg_time = result.compute_average_time_taken_by_stage()
+    print(f"Average time taken by stage: {avg_time}")
+    time_variance = result.compute_average_time_taken_variance_by_stage()
+    print(f"Average time taken variance by stage: {time_variance}")
 
 
 if __name__ == "__main__":
