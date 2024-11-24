04ca5ebf6deb663059bcd4b21232b80d9c5d303e
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2024-11-24 14:51:25 +0100

    Refactor TestStageResult and TestSuiteResult to include true negative metrics and enhance evaluation output

--- src/evaluation/results.py
+++ src/evaluation/results.py
@@ -8,19 +8,23 @@ from reader.method_signature import MethodSignature
 @dataclass
 class TestStageResult:
     predicted: Set[MethodSignature]
-    ground_truth: Set[MethodSignature]
+    ground_truth_positive: Set[MethodSignature]
+    ground_truth_negative: Set[MethodSignature]
     found_ground_truth: Set[MethodSignature]
     prediction_time: float = 0.0
     test_time: float = 0.0
 
     def compute_true_positives(self) -> Set[MethodSignature]:
-        return self.predicted & self.ground_truth
+        return self.predicted & self.ground_truth_positive
     
     def compute_false_positives(self) -> Set[MethodSignature]:
-        return self.predicted - self.ground_truth
+        return self.predicted - self.ground_truth_positive
     
     def compute_false_negatives(self) -> Set[MethodSignature]:
-        return self.ground_truth - self.predicted
+        return self.ground_truth_positive - self.predicted
+    
+    def compute_true_negatives(self) -> Set[MethodSignature]:
+        return self.ground_truth_negative - self.predicted
     
     def compute_true_positives_count(self) -> int:
         return len(self.compute_true_positives())
@@ -30,7 +34,9 @@ class TestStageResult:
     
     def compute_false_negatives_count(self) -> int:
         return len(self.compute_false_negatives())
-
+    
+    def compute_true_negatives_count(self) -> int:
+        return len(self.compute_true_negatives())
 
 @dataclass
 class TestScenarioResult:
@@ -59,11 +65,18 @@ class TestScenarioResult:
         for stage_result in self.stage_results:
             total_false_negative_count += stage_result.compute_false_negatives_count()
         return total_false_negative_count
+    
+    def compute_total_true_negative_count(self) -> int:
+        total_true_negative_count = 0
+        for stage_result in self.stage_results:
+            total_true_negative_count += stage_result.compute_true_negatives_count()
+        return total_true_negative_count
 
 
 @dataclass
 class TestSuiteResult:
     scenario_results: List[TestScenarioResult]
+    evaluation_time: float
     
     def compute_total_prediction_time(self) -> float:
         total_time_taken = 0.0
@@ -88,6 +101,12 @@ class TestSuiteResult:
         for scenario_result in self.scenario_results:
             total_false_negative_count += scenario_result.compute_total_false_negative_count()
         return total_false_negative_count
+    
+    def compute_total_true_negative_count(self) -> int:
+        total_true_negative_count = 0
+        for scenario_result in self.scenario_results:
+            total_true_negative_count += scenario_result.compute_total_true_negative_count()
+        return total_true_negative_count
     
     def compute_total_accuracy(self) -> float:
         total_true_positive_count = self.compute_total_true_positive_count()
@@ -183,16 +202,24 @@ class TestSuiteResult:
         return stage_count
 
     def print_stats(self):
-        print(f"Total time taken: {self.compute_total_prediction_time()}")
-        print(f"Total stage count: {self.compute_total_stage_count()}")
-        print(f"Mean prediction time: {self.compute_mean_prediction_time()}")
-        print(f"Prediction time variance: {self.compute_prediction_time_variance()}")
-        print(f"Mean test time: {self.compute_mean_test_time()}")
-        print(f"Test time variance: {self.compute_test_time_variance()}")
-        print(f"Total true positives: {self.compute_total_true_positive_count()}")
-        print(f"Total false positives: {self.compute_total_false_positive_count()}")
-        print(f"Total false negatives: {self.compute_total_false_negative_count()}")
-        print(f"Total accuracy: {self.compute_total_accuracy()}")
-        print(f"Total precision: {self.compute_total_precision()}")
-        print(f"Total recall: {self.compute_total_recall()}")
-        print(f"Total F1 score: {self.compute_total_f1_score()}")
+        print(f"===== Evaluation Results =====")
+        print(f"-- Time --")
+        print(f" Total stage count: {self.compute_total_stage_count()}")
+        print(f" Total evaluation time: {self.evaluation_time}")
+        print(f" Total prediction time: {self.compute_total_prediction_time()}")
+        print(f" Mean prediction time: {self.compute_mean_prediction_time()}")
+        print(f" Prediction time variance: {self.compute_prediction_time_variance()}")
+        print(f" Mean test time: {self.compute_mean_test_time()}")
+        print(f" Test time variance: {self.compute_test_time_variance()}")
+        print(f"-- Confusion Matrix Metrics --")
+        print(f" Total true positives: {self.compute_total_true_positive_count()}")
+        print(f" Total false positives: {self.compute_total_false_positive_count()}")
+        print(f" Total false negatives: {self.compute_total_false_negative_count()}")
+        print(f" Total true negatives: {self.compute_total_true_negative_count()}")
+        print(f" In total {self.compute_total_true_positive_count() + self.compute_total_false_positive_count() + self.compute_total_false_negative_count() + self.compute_total_true_negative_count()} tests")
+        print(f"-- Evaluation Metrics --")
+        print(f" Total accuracy: {self.compute_total_accuracy()}")
+        print(f" Total precision: {self.compute_total_precision()}")
+        print(f" Total recall: {self.compute_total_recall()}")
+        print(f" Total F1 score: {self.compute_total_f1_score()}")
+        print(f"=============================")
